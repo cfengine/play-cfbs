56d12461dbff1b2243cfb5ceb0c52ea4ac6d13f5
Craig Comstock <craig.comstock@northern.tech>
2024-04-24 12:23:05 -0500

    Craigs-MacBook-Pro.local

--- setup.sh
+++ setup.sh
@@ -1,21 +1,34 @@
+# pauses in stages
+#!/usr/bin/env bash
+set -ex
 server=testhub
-clients=debian-10 centos-7
-hosts=$server $clients
+cf-remote destroy --all
+cf-remote save --role hub --name $server --hosts vagrant@$server
+cf-remote sudo -H $server "apt install -y python3-pip; pip3 install cfbs"
+
+clients="debian-10 centos-7"
+for client in $clients; do
+  cf-remote save --role client --name $client --hosts vagrant@$client
+done
+
+hosts="$server $clients"
 policy_server_ip=192.168.56.202
 
 for host in $hosts; do
-  cf-remote uninstall -H vagrant@$host
+  cf-remote uninstall -H $host
 done
 
-cf-remote install --edition community --client vagrant@$server --bootstrap $policy_server_ip
-cf-remote sudo -H vagrant@$server "curl --silent https://raw.githubusercontent.com/cfengine/core/master/contrib/masterfiles-stage/install-masterfiles-stage.sh --remote-name"
-cf-remote sudo -H vagrant@$server "chmod +x install-masterfiles-stage.sh"
-cf-remote sudo -H vagrant@$server "./install-masterfiles-stage.sh"
-cf-remote scp -H vagrant@$server params.sh /opt/cfengine/dc-scripts/params.sh
-cf-remote scp -H vagrant@$server cfe /usr/bin/
-cf-remote sudo -H vagrant@$server cfe
-exit 0
+cf-remote install --edition community --clients $server --bootstrap $policy_server_ip
+cf-remote sudo -H $server "curl --silent https://raw.githubusercontent.com/cfengine/core/master/contrib/masterfiles-stage/install-masterfiles-stage.sh --remote-name"
+cf-remote sudo -H $server "chmod +x install-masterfiles-stage.sh"
+cf-remote sudo -H $server "./install-masterfiles-stage.sh"
+cf-remote scp -H $server params.sh 
+cf-remote sudo -H $server "cp /home/vagrant/params.sh /opt/cfengine/dc-scripts/"
+cf-remote sudo -H $server "/var/cfengine/httpd/htdocs/api/dc-scripts/masterfiles-stage.sh --DEBUG"
+cf-remote scp -H $server cfe 
+cf-remote sudo -H $server "cp /home/vagrant/cfe /usr/bin/cfe; chmod +x /usr/bin/cfe"
+cf-remote sudo -H $server cfe
 
 for client in $clients; do
-  cf-remote install --edition community --client vagrant@$client --bootstrap $policy_server_ip
+  cf-remote install --edition community --clients $client --bootstrap $policy_server_ip
 done
