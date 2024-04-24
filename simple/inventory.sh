ce3bef8d1c45f6de5f33f9d204bd2ceef151f100
Craig Comstock <craig.comstock@northern.tech>
2024-04-24 11:01:20 -0500

    Added module './simple/'

---
+++ simple/inventory.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+for client_ip in $(cf-key -s | grep Incoming | awk '{print $2}'); do
+  echo "client ip: $client_ip"
+  for dir in $(cf-net -H $client_ip opendir /var/cfengine/data | grep -v '^\.'); do
+    for file in $(cf-net -H $client_ip opendir /var/cfengine/data/$dir | grep -v '^\.'); do
+      mkdir -p /var/cfengine/data/$dir
+      cf-net -H $client_ip get /var/cfengine/data/$dir/$file
+    done
+  done
+done
